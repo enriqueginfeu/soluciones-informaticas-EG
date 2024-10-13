83bf541abebf8ae60ffb9fc51ebdfdfce906de93
Enrique Ginfeu <ginfeu.enrique@hotmail.com>
2024-10-13 20:16:36 -0300

    agregando componentes

---
+++ src/reparaciones.js
@@ -0,0 +1,52 @@
+const reparaciones = [
+    {
+        id: 1,
+        titulo: "Cambio de bateria",
+        descripcion: "La reparación del Moto E6i fue un proceso sencillo pero crucial, ya que la batería original se descargaba rápidamente, afectando su rendimiento. Al reemplazar la batería por una nueva y de calidad, el teléfono recuperé su autonomía, permitiendo al usuario disfrutar de largas horas de uso sin interrupciones. Este cambio no solo mejoró la duración de la carga, sino que también optimizó el funcionamiento general del dispositivo, devolviéndole su agilidad y eficiencia.",
+        img: "/reparacion_01.webp"
+    },
+    {
+        id: 3,
+        titulo: "Cambio de circuito",
+        descripcion: "El cambio del circuito flex membrana del joystick de PS4 resultó ser la solución perfecta para las fallas en los botones L2 y R2, que dificultaban la experiencia de juego. Al desmontar el controlador y reemplazar la membrana dañada, se restableció la conectividad y la sensibilidad de los botones. Después de la reparación, el joystick volvió a funcionar como nuevo, permitiendo al jugador disfrutar de sus títulos favoritos sin interrupciones. Este sencillo pero efectivo cambio prolongó la vida útil del controlador y mejoró notablemente el rendimiento durante las partidas.",
+        img: "/reparacion_03.webp"
+    },
+    {
+        id: 4,
+        titulo: "Cambio de disco rigido a SSD",
+        descripcion: "El cambio del disco duro a un SSD en la notebook Samsung transformó por completo su rendimiento. Al reemplazar el antiguo HDD, la velocidad de arranque y la carga de aplicaciones se incrementaron significativamente. Además, se realizó una instalación limpia de Windows 10, asegurando que el sistema operativo estuviera optimizado y actualizado con las últimas mejoras de seguridad y rendimiento. El resultado fue una experiencia de uso mucho más ágil y fluida, permitiendo al usuario disfrutar de un dispositivo revitalizado que respondía con rapidez a cada comando. Esta actualización no solo mejoró la velocidad, sino que también prolongó la vida útil de la notebook.",
+        img: "/reparacion_04.webp"
+    },
+    {
+        id: 5,
+        titulo: "Limpieza y cambio de potenciometros",
+        descripcion: "El cambio de los potenciómetros en el joystick de PS4 Pro fue fundamental para solucionar el molesto problema de drift en los análogos, que afectaba la precisión en los juegos. Tras reemplazar los componentes desgastados, el controlador recuperó su respuesta exacta y fluidez en el manejo. Posteriormente, se realizó una limpieza exhaustiva de todos los componentes internos, eliminando el polvo y la suciedad acumulada que podían interferir en su funcionamiento. Esta combinación de reparaciones no solo eliminó el drift, sino que también garantizó un rendimiento óptimo y duradero del joystick, permitiendo al jugador disfrutar de una experiencia de juego sin interrupciones.",
+        img: "/reparacion_05.webp"
+    },
+    {
+        id: 6,
+        titulo: "cambio de bateria",
+        descripcion: "El cambio de batería en la tablet Samsung fue la solución perfecta para el problema de no retener carga, que había vuelto el dispositivo prácticamente inutilizable. Al reemplazar la batería defectuosa por una nueva, la tablet recuperó su capacidad para mantener la carga y funcionar durante largas sesiones. Este cambio no solo revitalizó el dispositivo, permitiendo al usuario disfrutar de su contenido sin preocupaciones, sino que también mejoró el rendimiento general, asegurando que la tablet esté lista para cualquier tarea diaria. La reparación resultó en una experiencia más satisfactoria y duradera para el usuario.",
+        img: "/reparacion_06.webp"
+    },
+    {
+        id: 7,
+        titulo: "Limpieza y fallas en los botones",
+        descripcion: "La reparación del joystick de PS3 que presionaban todos los botones automáticamente fue un éxito al identificar que el problema radicaba en un mal contacto en el circuito. Al ajustar y reparar el circuito afectado, se restauró la funcionalidad adecuada de cada botón. Después de resolver la falla, se realizó una limpieza completa de los componentes internos, eliminando el polvo y cualquier residuo que pudiera afectar el rendimiento. Con esta combinación de reparaciones, el joystick volvió a funcionar perfectamente, ofreciendo al usuario una experiencia de juego fluida y sin interrupciones.",
+        img: "/reparacion_07.webp"
+    },
+    {
+        id: 9,
+        titulo: "Cambio de disco rigido a SSD",
+        descripcion: "El cambio del disco duro a un SSD en la notebook Acer Aspire 3 marcó una notable mejora en su rendimiento. Al reemplazar el viejo HDD, la velocidad de arranque y la carga de aplicaciones se dispararon, transformando la experiencia del usuario. Además, se llevó a cabo una instalación limpia de Windows 10, asegurando que el sistema operativo estuviera completamente optimizado y con las últimas actualizaciones de seguridad. Con estas mejoras, la notebook adquirió una agilidad impresionante, permitiendo al usuario disfrutar de un funcionamiento rápido y eficiente, revitalizando así un dispositivo que, a pesar de su antigüedad, pudo seguir cumpliendo con sus tareas diarias de manera excepcional.",
+        img: "/reparacion_09.webp"
+    },
+    {
+        id: 10,
+        titulo: "Limpieza y cambio de pasta termica",
+        descripcion: "El cambio de pasta térmica en la consola PS3 Fat fue fundamental para solucionar el problema de sobrecalentamiento que provocaba la notificación de advertencia durante el juego. Al reemplazar la pasta desgastada, se mejoró significativamente la transferencia de calor entre el procesador y el disipador. Después de esta intervención, se realizó una limpieza profunda de la consola, eliminando el polvo y la suciedad acumulada que afectaban su ventilación. Como resultado, la PS3 volvió a funcionar de manera eficiente, permitiendo largas sesiones de juego sin preocupaciones por el calor, y prolongando así la vida útil del dispositivo.",
+        img: "/reparacion_10.webp"
+    },
+]
+
+export default reparaciones;
